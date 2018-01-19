72a955ba20420ad6eb128bfa5d46cbc5171cca1b
David Surya Aji Saputra <dapidsurya@gmail.com>
2018-01-19 10:30:59 +0700

    home url

--- resources/views/content/partials/header.blade.php
+++ resources/views/content/partials/header.blade.php
@@ -2,7 +2,7 @@
   <header class="blog-header py-3">
     <div class="row flex-nowrap justify-content-between align-items-center">
       <div class="col-4 pt-1">
-        <a class="blog-header-logo text-dark" href="#">Sociolla Blog Apps</a>
+        <a class="blog-header-logo text-dark" href="{{ url('/') }}">Sociolla Blog Apps</a>
       </div>
 
       <div class="col-4 text-center">
